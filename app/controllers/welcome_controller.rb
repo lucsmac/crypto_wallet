53d12dd5248bb0b9b45d0cddfeca65f6b6a53e05
Lucas Macedo <lucasmdc57@gmail.com>
2021-03-01 08:52:16 -0300

    layouts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     #variáveis de sessão
-    @nome = params[:nome]
-    @curso = params[:curso]
+    @nome = params[:nome] || 'Lucas'
+    @curso = params[:curso] || 'Rails'
   end
 end
