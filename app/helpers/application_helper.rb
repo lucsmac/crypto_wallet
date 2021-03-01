c13f3eafc7255da84d5cc3db707f665c5ecda273
Lucas Macedo <lucasmdc57@gmail.com>
2021-02-28 21:06:48 -0300

    Rails environments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def date_br(date_us)
     date_us.strftime('%d/%m/%Y')
   end
+
+  def rails_env
+    if Rails.env.development?
+      "desenvolvimento"
+    elsif Rails.env.production?
+      "produção"
+    else Rails.env.test?
+      "teste"
+    end
+  end
 end
