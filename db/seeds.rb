53c12bb3e4fb9f685d0da682196916cb844fe80a
Lucas Macedo <lucasmdc57@gmail.com>
2021-03-02 11:37:07 -0300

    refactoring seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Creating coins..."
+coins = [
+  {
+    description: 'Bitcoin',
+    acronym: 'BTC',
+    url_image: 'https://i.pinimg.com/originals/ef/da/8b/efda8b6316786f2ff349f3065974249b.jpg'
+  },
+  {
+    description: 'Ethereum',
+    acronym: 'ETH',
+    url_image: 'https://marcas-logos.net/wp-content/uploads/2020/03/ETHEREUM-LOGO.png'
+  }
+]
 
-Coin.create!(
-  description: 'Bitcoin',
-  acronym: 'BTC',
-  url_image: 'https://i.pinimg.com/originals/ef/da/8b/efda8b6316786f2ff349f3065974249b.jpg'
-)
-
-Coin.create!(
-  description: 'Ethereum',
-  acronym: 'ETH',
-  url_image: 'https://marcas-logos.net/wp-content/uploads/2020/03/ETHEREUM-LOGO.png'
-)
-
-puts "Coins created!"
+spinner = TTY::Spinner.new("[:spinner] Creating coins...")
+spinner.auto_spin
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
+spinner.success("(Coins created!)")
