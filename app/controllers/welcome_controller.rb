7e1a466e3598f0fd092978dea3f35e047c8fcdfa
Lucas Macedo <lucasmdc57@gmail.com>
2021-03-04 18:39:46 -0300

    add toggle language feature

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     #variáveis de sessão
+    # cookies[:curso] = "Curso de Ruby on Rails"
+    # session[:curso] = "Curso de Ruby on Rails"
     @nome = params[:nome] || 'Lucas'
     @curso = params[:curso] || 'Rails'
   end
